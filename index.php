6c66210fc91794662fd3f86a3791685f08a4dcae
Safia Nassiri <niejelandrew@gmail.com>
2025-05-17 21:05:08 -0400

    Updated the social media logos [NEEDS MORE WORK]

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <?php include 'includes/header.php'; ?>
 <div class="container">
+
+  <section class="social-media">
+    <h2>Follow Us on Social Media</h2>
+    <p>Stay connected and get the latest updates from With Special Guests:</p>
+    <ul class="social-links">
+      <li>
+        <a href="https://facebook.com/WithSpecialGuests" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="#1877F2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22 12.07C22 6.48 17.52 2 12 2S2 6.48 2 12.07c0 4.97 3.66 9.07 8.44 9.86v-6.98h-2.54v-2.88h2.54v-2.2c0-2.5 1.49-3.88 3.77-3.88 1.09 0 2.23.2 2.23.2v2.46h-1.26c-1.24 0-1.63.77-1.63 1.56v1.87h2.78l-.44 2.88h-2.34v6.98C18.34 21.14 22 17.04 22 12.07z"/>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="https://instagram.com/WithSpecialGuests" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="#E4405F" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5a4.25 4.25 0 004.25-4.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zm8.75 2a1 1 0 110 2 1 1 0 010-2zM12 7a5 5 0 110 10 5 5 0 010-10zm0 1.5a3.5 3.5 0 100 7 3.5 3.5 0 000-7z"/>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/WithSpecialGuests" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="#1DA1F2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M23 4.56a9.93 9.93 0 01-2.83.77 4.93 4.93 0 002.17-2.72 9.87 9.87 0 01-3.13 1.2 4.92 4.92 0 00-8.39 4.48A13.97 13.97 0 013 3.16a4.92 4.92 0 001.52 6.56 4.9 4.9 0 01-2.23-.62v.06a4.92 4.92 0 003.95 4.82 4.94 4.94 0 01-2.22.08 4.92 4.92 0 004.59 3.42A9.86 9.86 0 013 19.54a13.9 13.9 0 007.56 2.21c9.05 0 14-7.5 14-14 0-.21 0-.42-.02-.63A10.03 10.03 0 0023 4.56z"/>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="https://youtube.com/WithSpecialGuests" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="#FF0000" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 15l5.19-3L10 9v6zm12-3c0 1.1-.89 2-2 2h-1.17l-1.56 2.08c-.14.18-.35.29-.57.29H8.3c-.22 0-.43-.11-.57-.29L6.17 14H5c-1.11 0-2-.9-2-2V8c0-1.1.89-2 2-2h1.17l1.56-2.08c.14-.18.35-.29.57-.29h7.4c.22 0 .43.11.57.29L16.83 6H18c1.11 0 2 .9 2 2v4z"/>
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </section>
+
   <section>
     <h2>Welcome to Our Official Website</h2>
     <p>Check out our latest album and upcoming tour dates!</p>
   </section>
+
 </div>
 <?php include 'includes/footer.php'; ?>
