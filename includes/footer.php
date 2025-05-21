16348ede6c473973f595ac6604a43cbec2fc8b02
Gknowsss <geecostan@gmail.com>
2025-05-20 21:01:29 -0400

    Tiktok link works

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <ul class="footer-social">
       <li><a href="https://www.instagram.com/with_special_guests" target="_blank">Instagram</a></li>
       <li><a href="https://www.youtube.com/channel/UC02LUQQ1jD4qYK9xmIN0F2A" target="_blank">YouTube</a></li>
-      <li><a href="#" target="_blank">TikTok</a></li>
+      <li><a href="https://www.tiktok.com/@withspecialguests" target="_blank">TikTok</a></li>
       <li><a href="https://withspecialguests.bandcamp.com" target="_blank">BandCamp</a></li>
     </ul>
     <p>&copy; Copyright With Special Guests. All rights reserved.</p>
